ab9d4d465b7bf5fe7a708969664f19d4e06adcc4
Rafael Santos <rafaelsantos1983@gmail.com>
2025-02-11 20:58:15 -0300

    Update editor.tsx

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -2,6 +2,7 @@ import { AsideNav } from "../components/aside-nav";
 import { Editor } from "../components/editor/editor";
 import { PageWrapper } from "../components/page-wrapper";
 
+// comentario
 export function EditorPage(){
   return(
     <PageWrapper>
